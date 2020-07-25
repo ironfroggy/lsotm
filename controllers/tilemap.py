14177f37cc7af982672b25725058bb8ecdc77788
Calvin Spealman <ironfroggy@gmail.com>
2020-07-25 14:54:07 -0400

    River!

--- controllers/tilemap.py
+++ controllers/tilemap.py
@@ -36,6 +36,7 @@ def make_mapitem(image, rect, mapitemtype):
 
 SOLID = 1
 DECOR = 2
+GROUND = 3
 GROUND_IMAGES = [
     ppb.Image(f"resources/ground/ground_{r}.png")
     for r in range(6)
@@ -43,6 +44,9 @@ GROUND_IMAGES = [
 TREE_H = ppb.Image("resources/ground/tree_horizontal.png")
 TREE_V = ppb.Image("resources/ground/tree_vertical.png")
 ROCKS = ppb.Image("resources/ground/rocks.png")
+BRIDGE = ppb.Image("resources/ground/bridge.png")
+RIVER = ppb.Image("resources/ground/river.png")
+
 EXIT = ppb.Image("resources/ground/exit.png")
 
 ITEMS = {
@@ -52,9 +56,25 @@ ITEMS = {
     't4': make_mapitem(TREE_H, (0,  0, 32, 32), SOLID),
     't5': make_mapitem(TREE_H, (32, 0, 32, 32), SOLID),
     't6': make_mapitem(TREE_H, (64, 0, 32, 32), SOLID),
+    
     'r1': make_mapitem(ROCKS, (0,  0, 32, 32), SOLID),
     'r2': make_mapitem(ROCKS, (32, 0, 32, 32), SOLID),
     'r3': make_mapitem(ROCKS, (64, 0, 32, 32), SOLID),
+
+    'w1': make_mapitem(RIVER, (0, 0, 32, 32), SOLID),
+    'w2': make_mapitem(RIVER, (32, 0, 32, 32), SOLID),
+    'w3': make_mapitem(RIVER, (64, 0, 32, 32), SOLID),
+
+    'b1': make_mapitem(BRIDGE, (0, 0, 32, 32), SOLID),
+    'b2': make_mapitem(BRIDGE, (32, 0, 32, 32), SOLID),
+    'b3': make_mapitem(BRIDGE, (64, 0, 32, 32), SOLID),
+    'b4': make_mapitem(BRIDGE, (0, 32, 32, 32), GROUND),
+    'b5': make_mapitem(BRIDGE, (32, 32, 32, 32), GROUND),
+    'b6': make_mapitem(BRIDGE, (64, 32, 32, 32), GROUND),
+    'b7': make_mapitem(BRIDGE, (0, 64, 32, 32), GROUND),
+    'b8': make_mapitem(BRIDGE, (32, 64, 32, 32), GROUND),
+    'b9': make_mapitem(BRIDGE, (64, 64, 32, 32), GROUND),
+
     'EX': make_mapitem(EXIT, (0, 0, 32, 32), DECOR),
 }
 
@@ -77,7 +97,7 @@ class TilemapCtrl:
         self.width = width
         self.height = height
         self.images = images
-        self.tiles = []
+        self.tiles = {}
         self.objects = {}
         self.pathfinder = AStarPathFinder(50)
     
@@ -90,7 +110,7 @@ class TilemapCtrl:
                     layer=self.layer,
                     size=1.01,
                 )
-                self.tiles.append(t)
+                self.tiles[x, y] = t
                 scene.add(t)
         
         # Load map objects, which can be:
@@ -113,9 +133,16 @@ class TilemapCtrl:
                 else:
                     t = factory(cell.x, cell.y)
                     if t:
-                        scene.add(t, tags=[t.tag])
-                        self.objects[cell.x, cell.y] = t
-                        self.pathfinder.block((cell.x, cell.y))
+                        if t.mapitemtype == DECOR:
+                            scene.add(t, tags=[t.tag])
+                            self.objects[cell.x, cell.y] = t
+                        elif t.mapitemtype == SOLID:
+                            scene.add(t, tags=[t.tag])
+                            self.pathfinder.block((cell.x, cell.y))
+                        elif t.mapitemtype == GROUND:
+                            self.tiles[cell.x, cell.y].image = t.image
+                            self.tiles[cell.x, cell.y].rect = t.rect
+                            self.tiles[cell.x, cell.y].size = t.size
         signal(LevelLoaded(scene.level, level))
 
     
